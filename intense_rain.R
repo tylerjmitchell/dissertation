d6a7d3b6903b2d936d20a7ae611eafc5f009d2aa
tylerjmitchell <18473796+tylerjmitchell@users.noreply.github.com>
2022-03-31 02:00:54 +0000

    Update and rename intenserain.R to intense_rain.R

--- intense_rain.R
+++ intense_rain.R
@@ -33,3 +33,26 @@ ire.precipitation <- setNames(aggregate(data$ireppt ~ data$year, FUN = sum),
 
 ire <- merge(ire.frequency, ire.precipitation, by = "year")
 ire
+
+# another workflow using the lubridate package and pipes to calculate all years 
+# and months at once. calculates standard normal (z) scores based on ALL months distribution ----
+require("lubridate")
+require("tidyverse")
+data <- read.csv("2917741.csv")
+
+# remove missing precipitation observations ----
+data <- data[!is.na(data$PRCP),]
+
+# standard-normal (z) scores for precipitation and selection of intensity threshold ----
+data$z <- scale(data$PRCP)
+threshold <- 2.00
+data$ire <- ifelse(data$z > threshold, 1, 0)
+
+# retain precipitation amounts from intense rainfall event (ire) selection ----
+data$ireppt <- ifelse(data$ire == 1, data$PRCP, 0)
+
+data <- data %>% 
+  group_by(year = year(DATE), month = month(DATE)) %>% 
+  summarise_if(is.numeric, sum)
+
+data[c(1:2,6,8:9)]
